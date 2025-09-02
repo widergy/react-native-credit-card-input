15a03d6c25716a32a51e1e41ea72fd9fe0aa9961
alcurvelo <alberto.curvelo@widergy.com>
2025-09-02 13:04:22 -0300

    fix: refs and proptypes

--- src/CCInput.js
+++ src/CCInput.js
@@ -1,18 +1,9 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
-import {
-  View,
-  Text,
-  TextInput,
-  TouchableOpacity,
-  StyleSheet,
-  ViewPropTypes,
-} from "react-native";
+import { View, Text, TextInput, TouchableOpacity, StyleSheet } from "react-native";
 
 const s = StyleSheet.create({
-  baseInputStyle: {
-    color: "black",
-  },
+  baseInputStyle: { color: "black" },
 });
 
 export default class CCInput extends Component {
@@ -25,9 +16,9 @@ export default class CCInput extends Component {
 
     status: PropTypes.oneOf(["valid", "invalid", "incomplete"]),
 
-    containerStyle: ViewPropTypes.style,
-    inputStyle: Text.propTypes.style,
-    labelStyle: Text.propTypes.style,
+    containerStyle: PropTypes.any,
+    inputStyle: PropTypes.any,
+    labelStyle: PropTypes.any,
     validColor: PropTypes.string,
     invalidColor: PropTypes.string,
     placeholderColor: PropTypes.string,
@@ -40,7 +31,7 @@ export default class CCInput extends Component {
     cancelScrollOnValidNumber: PropTypes.bool,
     secureTextEntry: PropTypes.bool,
     isLast: PropTypes.bool,
-    additionalInputProps: PropTypes.shape(TextInput.propTypes),
+    additionalInputProps: PropTypes.object,
   };
 
   static defaultProps = {
@@ -57,6 +48,11 @@ export default class CCInput extends Component {
     additionalInputProps: {},
   };
 
+  constructor(props) {
+    super(props);
+    this.inputRef = React.createRef();
+  }
+
   componentWillReceiveProps = newProps => {
     const { status, value, onBecomeEmpty, onBecomeValid, field, cancelScrollOnValidNumber } = this.props;
     const { status: newStatus, value: newValue } = newProps;
@@ -65,36 +61,66 @@ export default class CCInput extends Component {
     if (status !== "valid" && newStatus === "valid" && !cancelScrollOnValidNumber) onBecomeValid(field);
   };
 
-  focus = () => this.refs.input.focus();
+  focus = () => {
+    if (this.inputRef && this.inputRef.current && typeof this.inputRef.current.focus === "function") {
+      this.inputRef.current.focus();
+    }
+  };
 
   _onFocus = () => this.props.onFocus(this.props.field);
   _onChange = value => this.props.onChange(this.props.field, value);
   _onSubmitEditing = value => this.props.onSubmitEditing(this.props.field, value);
 
   render() {
-    const { label, value, placeholder, status, keyboardType,
-            containerStyle, inputStyle, labelStyle,
-            validColor, invalidColor, placeholderColor,
-            additionalInputProps, secureTextEntry, isLast } = this.props;
+    const {
+      label,
+      value,
+      placeholder,
+      status,
+      keyboardType,
+      containerStyle,
+      inputStyle,
+      labelStyle,
+      validColor,
+      invalidColor,
+      placeholderColor,
+      additionalInputProps,
+      secureTextEntry,
+      isLast,
+    } = this.props;
+    let restAdditionalProps = {};
+    let additionalStyle = undefined;
+    if (additionalInputProps && typeof additionalInputProps === "object") {
+      additionalStyle = additionalInputProps.style;
+      restAdditionalProps = Object.keys(additionalInputProps).reduce((acc, key) => {
+        if (key !== "ref" && key !== "style") {
+          return { ...acc, [key]: additionalInputProps[key] };
+        }
+        return acc;
+      }, {});
+    }
+
     return (
-      <TouchableOpacity onPress={this.focus}
-        activeOpacity={0.99}>
+      <TouchableOpacity onPress={this.focus} activeOpacity={0.99}>
         <View style={[containerStyle]}>
-          { !!label && <Text style={[labelStyle]}>{label}</Text>}
-          <TextInput ref="input"
-            {...additionalInputProps}
+          {!!label && <Text style={[labelStyle]}>{label}</Text>}
+          <TextInput
+            {...restAdditionalProps}
+            ref={this.inputRef}
             keyboardType={keyboardType}
-            autoCapitalise="words"
+            autoCapitalize="words"
             autoCorrect={false}
             secureTextEntry={secureTextEntry}
             returnKeyType={isLast ? "done" : "next"}
             style={[
               s.baseInputStyle,
               inputStyle,
-              ((validColor && status === "valid") ? { color: validColor } :
-              (invalidColor && status === "invalid") ? { color: invalidColor } :
-              {}),
-              additionalInputProps.style,
+              (validColor && status === "valid")
+                ? { color: validColor }
+                : (invalidColor && status === "invalid")
+                ? { color: invalidColor }
+                : {},
+              additionalStyle,
             ]}
             underlineColorAndroid={"transparent"}
             placeholderTextColor={placeholderColor}
